32a43e55c03867bb35f0a3715b21d0d29262a122
Christian Shadis <cshadis@worcester.edu>
2019-10-26 14:15:59 -0400

    quick update

--- Dequeue.java
+++ Dequeue.java
@@ -7,24 +7,24 @@ import java.util.NoSuchElementException;
  * @Version 1.2
  * 
  */
-public class Dequeue<E> extends java.lang.Object{
+public class Dequeue<E> extends java.lang.Object {
 	private int front, rear;
 	E[] elements;
 	private static final int INITIAL_CAPACITY = 5;
 
 	/*
-	 * Creates an empty Dequeue
-	 * Running time O(1)
+	 * Creates an empty Dequeue Running time O(1)
 	 */
 	@SuppressWarnings("unchecked")
-	public Dequeue(){
+	public Dequeue() {
 		elements = (E[]) new Object[INITIAL_CAPACITY];
 		front = 0;
 		rear = -1;
 	}
-	
+
 	/*
 	 * Creates a larger array containing the same elements in the same order
+	 * 
 	 * @post: elements is a larger array and front and rear are reset
 	 * 
 	 * Running time O(n)
@@ -35,145 +35,177 @@ public class Dequeue<E> extends java.lang.Object{
 		E[] temp = (E[]) new Object[newSize];
 		int i = front;
 		for (int j = 0; j < elements.length; j++) {
-			temp [j] = elements[i % elements.length];
+			temp[j] = elements[i % elements.length];
 			i++;
 		}
 		front = 0;
 		rear = elements.length - 2;
 		elements = temp;
-					
+
 	}
+
 	/*
 	 * Adds an item to the front of the queue
+	 * 
 	 * @param anEntry is the item to add to the beginning of the dequeue
-	 * @return true
-	 * Running time O(1) NOT counting time for the reallocate method
+	 * 
+	 * @return true Running time O(1) NOT counting time for the reallocate method
 	 */
 	public boolean addFront(E anEntry) {
-		if ((rear + 2) % elements.length == front) reallocate();
+		if ((rear + 2) % elements.length == front)
+			reallocate();
 		if (size() == 0) {
 			elements[front] = anEntry;
 			rear++;
-		}
-		else {
-			if(front <= 0) 
+		} else {
+			if (front <= 0)
 				front = elements.length - 1 - front; // moves front to the last element in the array
-			else 
+			else
 				front--;
 			elements[front] = anEntry;
 		}
 		return true;
 	}
-	
+
 	/*
 	 * Adds an item to the rear of the queue
+	 * 
 	 * @param anEntry is the item to add to the beginning of the dequeue
-	 * @return true
-	 * Running time O(1) NOT counting time for the reallocate method
+	 * 
+	 * @return true Running time O(1) NOT counting time for the reallocate method
 	 */
 	public boolean addRear(E anEntry) {
-		if ((rear + 2) % elements.length == front) reallocate();
+		if ((rear + 2) % elements.length == front)
+			reallocate();
 		rear++;
 		elements[rear] = anEntry;
 		return true;
 	}
-	
+
 	/*
 	 * @pre: The dequeue is not empty
-	 * @post: the front item on the dequeue has been removed and the queue has one fewer element
-	 * Removes and returns the element in the front of the dequeue
+	 * 
+	 * @post: the front item on the dequeue has been removed and the queue has one
+	 * fewer element Removes and returns the element in the front of the dequeue
+	 * 
 	 * @throws NoSuchElementException if the dequeue is empty
+	 * 
 	 * @return the element that was in the front position of the dequeue
 	 */
 	public E removeFront() {
-		if(size() == 0) throw new NoSuchElementException();
+		if (size() == 0)
+			throw new NoSuchElementException();
 		else {
 			E result = elements[front];
-			elements[front] = null;
 			front++;
 			return result;
 		}
 	}
-	
+
 	/*
 	 * @pre: the dequeue is not empty
-	 * @post: the rear item on the queue has been removed and the dequeue is one element shorter
-	 * Removes and returns the rear element in the dequeue
+	 * 
+	 * @post: the rear item on the queue has been removed and the dequeue is one
+	 * element shorter Removes and returns the rear element in the dequeue
+	 * 
 	 * @throws NoSuchElementException if the dequeue is empty
-	 * @return the element that was in the rear position of the dequeue
-	 * Running time O(n) since it calls size()
+	 * 
+	 * @return the element that was in the rear position of the dequeue Running time
+	 * O(n) since it calls size()
 	 */
 	public E removeRear() {
-		if(size() == 0) throw new NoSuchElementException();
+		if (size() == 0)
+			throw new NoSuchElementException();
 		else {
 			E result = elements[rear];
-			elements[rear] = null;
-			if (rear <= 0) 
+			if (rear <= 0)
 				front = elements.length - 1 - rear; // moves rear to the last element in the array
-			else 
+			else
 				rear--;
 			return result;
 		}
 	}
+
 	/*
-	 * Returns the object at the front of the dequeue without removal
-	 * post: dequeue unchanged
+	 * Returns the object at the front of the dequeue without removal post: dequeue
+	 * unchanged
+	 * 
 	 * @return the object at the front of the dequeue
-	 * @throws NoSuchElementException if the dequeue is empty
-	 * Running time O(n) since it calls size()
-     */
+	 * 
+	 * @throws NoSuchElementException if the dequeue is empty Running time O(n)
+	 * since it calls size()
+	 */
 	public E peekFront() {
-		if(size() == 0) throw new NoSuchElementException();
-		else return elements[front];
+		if (size() == 0)
+			throw new NoSuchElementException();
+		else
+			return elements[front];
 	}
-	
+
 	/*
-	 * Returns the object at the rear of the dequeue without removal
-	 * post: dequeue unchanged
+	 * Returns the object at the rear of the dequeue without removal post: dequeue
+	 * unchanged
+	 * 
 	 * @return the object at the rear of the dequeue
-	 * @throws NoSuchElementException if the dequeue is empty
-	 * Running time O(n) since it calls size()
-     */
+	 * 
+	 * @throws NoSuchElementException if the dequeue is empty Running time O(n)
+	 * since it calls size()
+	 */
 	public E peekRear() {
-		if(size() == 0) throw new NoSuchElementException();
-		
-		else return elements[rear];
+		if (size() == 0)
+			throw new NoSuchElementException();
+
+		else
+			return elements[rear];
 	}
-	
+
 	/*
-	 * Checks if there are any elements in the dequeue
-	 * post: dequeue unchanged
-	 * @return whether it is empty
-	 * Running time O(n) since it calls size().
+	 * Checks if there are any elements in the dequeue post: dequeue unchanged
+	 * 
+	 * @return whether it is empty Running time O(n) since it calls size().
 	 */
 	public boolean empty() {
 		return (size() == 0);
 	}
-	
+
 	/*
 	 * Checks the size of the dequeue
+	 * 
 	 * @pre: none
+	 * 
 	 * @post: dequeue unchanged
-	 * @return number of elements in the dequeue
-	 * Running time O(n)
+	 * 
+	 * @return number of elements in the dequeue Running time O(n)
 	 */
 	public int size() {
-		int counter = 0;
-		for (int i = 0; i < elements.length; i++) {
-			if(elements[i] != null) {
-				counter ++;
+		if (rear == front - 1 || (rear == elements.length && front == 0))
+			return 0;
+		else if (rear >= front)
+			return (rear - front + 1);
+		else if (front > rear) {
+			if (rear < 0)
+				return 0;
+			else {
+				int counter = 0;
+				for (int j = front; j < elements.length; j++) {
+					counter++;
+				}
+				for (int j = 0; j <= rear; j++) {
+					counter++;
+				}
+				return counter;
 			}
-		}
-		return counter;
+		} else
+			return 0;
 	}
-	
+
 	/*
 	 * Returns an Iterator object
 	 */
-	public java.util.Iterator<E> iterator(){
+	public java.util.Iterator<E> iterator() {
 		return new myIterator();
 	}
-	
+
 	private class myIterator implements Iterator<E> {
 		// Data Fields
 		// Index of next element
@@ -220,10 +252,4 @@ public class Dequeue<E> extends java.lang.Object{
 
 	}
 }
-//line to verify this is the latest version of the code: 10/23/2019 3:42 PM
-
-
-	
-
-
-
+//line to verify this is the latest version of the code: 10/26/2019 2:12 PM
